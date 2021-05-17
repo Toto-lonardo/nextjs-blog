6388bed9dc7ab1d427c927848d1caa2af49984e7
Toto-lonardo <s.lonardo87@gmail.com>
2021-05-17 17:03:46 +0200

    Prove modifiche 1

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Gascoigne Pub is my favorite Pub</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is only a test app
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
